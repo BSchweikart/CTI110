f0510b9031fe5e6ae355ff51407f81caf7bf57a6
BSchweikart <31232164+BSchweikart@users.noreply.github.com>
2017-09-07 19:05:31 -0400

    Distance Traveled

---
+++ M2HW1_Schweikb0866.py
@@ -0,0 +1,26 @@
+#CTI-110
+#M2HW1
+#Schweikart, Brian
+#09/07/2017
+#
+
+# Find Distance Traveled from speed and Time
+print("Distance = Speed * Time")
+
+# Speed set at 70
+print("Speed is 70 MPH")
+speed = 70
+timeA = 6
+timeB = 10
+timeC = 15
+
+# Time A,B,C... ETC
+print("Time used will be 6 hrs, 10 hrs, and 15 hrs")
+distanceA = timeA * speed
+distanceB = timeB * speed
+distanceC = timeC * speed
+
+# Show Distance total
+print("Time was" ,timeA,"hr and distance covered was", distanceA,"miles")
+print("Time was" ,timeB,"hr and distance covered was", distanceB,"miles")
+print("Time was" ,timeC,"hr and distance covered was", distanceC,"miles")
